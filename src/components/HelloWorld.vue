3bdaafe52fbaa8e553fd23deb98c655ed4d016b1
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2019-09-09 21:45:51 -0600

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,75 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>{{ target.name }}</h1>
+    <h1 :class="checkHealth()">{{target.health}}</h1>
+    <h1 :class="target.health > 50 ? 'text-success' : 'text-danger'">{{target.health}}</h1>
+    <h1
+      :class="{'text-success' : true, 'text-warning' : target.health < 75, 'text-danger' : target.health < 50}"
+    >{{target.health}}</h1>
+    <h1>{{target.hits}}</h1>
+    <button class="btn btn-danger m-1" @click="punch();target.hits += 100">punch</button>
+    <button
+      class="btn btn-primary m-1"
+      v-for="item in items"
+      :key="item.name"
+      @click="addItem(item)"
+    >{{item.name}}</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: "Goku",
+  props: ["targetName"],
+  data() {
+    return {
+      target: {
+        name: this.targetName,
+        health: 100,
+        hits: 0,
+        inventory: []
+      },
+      items: [
+        {
+          name: "Shield",
+          mod: -0.5
+        },
+        {
+          name: "Sword",
+          mod: 0.5
+        }
+      ]
+    };
+  },
+  computed: {
+    modTotal() {
+      let total = 1;
+      this.target.inventory.forEach(i => {
+        total += i.mod;
+      });
+      return total;
+    }
+  },
+  methods: {
+    punch() {
+      this.target.health -= 1 * this.modTotal;
+      this.target.hits++;
+      this.target.inventory = [];
+    },
+    addItem(item) {
+      this.target.inventory.push(item);
+    },
+    checkHealth() {
+      let cl = "text-success";
+      if (this.target.health < 50) {
+        cl = "text.danger";
+      } else if (this.target.health < 80) {
+        cl = "text0warning";
+      }
+      return cl;
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img
+      alt="Goku"
+      src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d3e5c8b4-b0ab-4647-8a0b-6eb898bbc965/dcor3xg-701bf99f-3442-4225-a820-961d7c54facf.png/v1/fill/w_613,h_1304,strp/goku__super_saiyan_3__by_thetabbyneko_dcor3xg-pre.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MzQwMyIsInBhdGgiOiJcL2ZcL2QzZTVjOGI0LWIwYWItNDY0Ny04YTBiLTZlYjg5OGJiYzk2NVwvZGNvcjN4Zy03MDFiZjk5Zi0zNDQyLTQyMjUtYTgyMC05NjFkN2M1NGZhY2YucG5nIiwid2lkdGgiOiI8PTE2MDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.MxBmm-AQ0SFxD92pgrYB15CxxdPkCnWRIq9y05-a2Ag"
+      style="height:415px;"
+    />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     HelloWorld
   }
-}
+};
 </script>
